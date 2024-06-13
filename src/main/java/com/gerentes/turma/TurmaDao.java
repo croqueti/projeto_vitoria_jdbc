c10b0da3706d97b5f3962cc5a1e1ada35a068ab5
croqueti <vitoria.soares26@escola.pr.gov.br>
2024-06-13 11:50:29 -0300

    Upload da classe TurmaDao

---
+++ src/main/java/com/gerentes/turma/TurmaDao.java
@@ -0,0 +1,35 @@
+package com.gerentes.turma;
+import java.sql.PreparedStatement;
+import com.gerentes.conexao.Conexao;
+
+
+
+
+public class TurmaDao {
+    
+    public void cadastrarPessoa(TurmaPojo TurmaPojo) {
+
+        Conexao conexao = new Conexao();
+
+        String sql = "insert into turma(id, capacidade, horario) values (?, ?, ?)";
+
+        PreparedStatement ps = null;
+        
+        try {
+            ps = conexao.getConexao().prepareStatement(sql);
+    
+            ps.setInt(1, TurmaPojo.getId());
+            ps.setInt(2, TurmaPojo.getCapacidade());
+            ps.setInt(3, TurmaPojo.getHorario());
+          
+            ps.execute();
+            ps.close();
+
+            System.out.println("Dados inseridos com sucesso!");
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Ocorreu um erro com os dados.");
+        }
+    } 
+}
